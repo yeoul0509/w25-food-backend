990c2f89d0c8e4ed5ea8603612e4e0edce26da54
yeoul0509 <loss17338@gmail.com>
2025-12-06 15:25:21 +0900

    🗃️MongoDB 연결

---
+++ src/main/kotlin/kr/ac/kumoh/s20220636/w25_food_backend/W25FoodBackendApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20220636.w25_food_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class W25FoodBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<W25FoodBackendApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kumoh/s20220636/w25_food_backend/W25FoodBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20220636.w25_food_backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class W25FoodBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
